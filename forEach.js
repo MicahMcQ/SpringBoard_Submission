48dfd1f42d0ccd0a087a59088edb26f5a6e985bc
Micah McQueary <Misemc7@gmail.com>
2023-04-01 22:31:10 -0400

    forEach/map/filter practice

--- forEach.js
+++ forEach.js
@@ -1,17 +1,112 @@
-function add (x,y){
-    return x+y;
+const doubleValues = function(arr){
+    for(num in arr){
+        return num * 2
+    }
 }
 
-function subtract (x,y){
-    return x-y;
-}
+const onlyEvenValues =funciton(nums){
+    let newArr = [];
+    arr.forEach(function(num) {
+      if (num % 2 === 0) {
+        newArr.push(num);
+      }
+    });
+    return newArr;
+  }
 
-function multiply (x,y){
-    return x*y;
-}
+  function showFirstAndLast(arr) {
+    let arrs = [];
+    arr.forEach(function(val) {
+      arrs.push(val[0] + val[val.length - 1]);
+    });
+    return arrs;
+  }
 
-function divide (x,y){
-    return x/y;
-}
+  function addKeyAndValue(arr, key, value) {
+    arr.forEach(function(val) {
+      val[key] = value;
+    });
+    return arr;
+  }
 
-const mathFuncs(add,subtract,multiply,divide);
+  function vowelCount(str) {
+    let splitArr = str.split("");
+    let obj = {};
+    const vowels = "aeiou";
+  
+    splitArr.forEach(function(letter) {
+      let lowerCasedLetter = letter.toLowerCase()
+      if (vowels.indexOf(lowerCasedLetter) !== -1) {
+        if (obj[lowerCasedLetter]) {
+          obj[lowerCasedLetter]++;
+        } else {
+          obj[lowerCasedLetter] = 1;
+        }
+      }
+    });
+    return obj;
+  }
+  
+  const doubleValuesWithMap = funciton(arr){
+    return arr.map(function(item){
+        return item*2
+    })
+  }
+
+  function valTimesIndex(arr) {
+    return arr.map(function(val, idx) {
+      return val * idx;
+    });
+  }
+  function extractKey(arr, key) {
+    return arr.map(function(val) {
+      return val[key];
+    });
+  }
+  
+  function extractFullName(arr) {
+    return arr.map(function(val) {
+      return val.first + " " + val.last;
+    });
+  }
+  
+  // filter
+  
+  function filterByValue(arr, key) {
+    return arr.filter(function(val) {
+      return val[key] !== undefined;
+    });
+  }
+  
+  function find(arr, searchValue) {
+    return arr.filter(function(val) {
+      return val === searchValue;
+    })[0];
+  }
+  
+  function findInObj(arr, key, searchValue) {
+    return arr.filter(function(val) {
+      return val[key] === searchValue;
+    })[0];
+  }
+  
+  function removeVowels(str) {
+    const vowels = "aeiou";
+    return str
+      .toLowerCase()
+      .split("")
+      .filter(function(val) {
+        return vowels.indexOf(val) === -1;
+      })
+      .join("");
+  }
+  
+  function doubleOddNumbers(arr) {
+    return arr
+      .filter(function(val) {
+        return val % 2 !== 0;
+      })
+      .map(function(val) {
+        return val * 2;
+      });
+    }
